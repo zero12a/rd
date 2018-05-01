69d142e5a5836a70ff9187139673c2c5c7d83ee7
YOUNG IL KIM <zeroone@YOUNGui-MacBook-Air.local>
2018-05-02 08:36:22 +0900

    rd

---
+++ bo_frame.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>rd frame</title>
+
+
+
+  </head>
+
+  <body style="height:100%;margin:0px 0px 0px 0px;padding:0px 0px 0px 0px;">
+    <div style="height:100%;width:100%;background-color:yellow;">
+        <div style="float:left;width:100%;background-color:green;height:120px;text-align:center">Top</div>
+        <div style="float:left;width:120px;background-color:blue;height:100%;text-align:top">Left</div>
+        333
+    </div>
+  </body>
+</html>
