65d05e1a6dc81603fedd4e492cffce4981dd1c49
YOUNG IL KIM <zeroone@YOUNGui-MacBook-Air.local>
2018-06-05 08:43:21 +0900

    boot

--- bo_bootstrap4.php
+++ bo_bootstrap4.php
@@ -9,11 +9,13 @@
     <title>Dashboard Template for Bootstrap</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
 
     <!-- Custom styles for this template -->
-    <link href="https://getbootstrap.com/docs/4.0/examples/dashboard/dashboard.css" rel="stylesheet">
-
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 
     <style>
     
@@ -40,13 +42,34 @@
     </style>
 
     <script language=javascript>
-      function addTab(tId, tNm){
-        alert(1);
-        $("#tabsJustified").append('<li class="nav-item"><a href="" data-target="#' + tId + '" id="' + tId + '" data-toggle="tab" class="nav-link small text-uppercase">' + tNm + '</a><button class="close" type="button" onclick="closeTab(' + tId + ')">×</button></li>');      
+      function addTab(tId, tNm, tUrl){
+        alert("addTab = " + tId);
+        //tab
+        $("#tabsJustified").append('<li id="li_' + tId + '" class="nav-item"><a href="" data-target="#' + tId + 'Content" id="' + tId + '" data-toggle="tab" class="nav-link small text-uppercase">' + tNm + '</a><span><button class="close" type="button" onclick="closeTab(\'' + tId + '\')">×</button></span></li>');      
+      
+        //tabContent
+        tStr = '<div id="' + tId + 'Content" class="tab-pane fade active show h-100" style="background:silver;overflow:visible" >';
+        tStr += '<iframe src="' + tUrl + '" style="overflow:visible" allowfullscreen="allowFullScreen" frameborder="0" width="100%" height="100%" style="background-color:gray;"></iframe>';
+        tStr += '</div>';
+        $("#tabsJustifiedContent").append(tStr);
+
+        //탭활성화 하기
+        //setTimeout(function() {
+          $("#tabsJustified > #li_" + tId + " a").tab('show');
+        //}, 1000);
+
+        //$("#tabsJustified > #li_" + tId).tab('show');
+        
+        //탭 이벤트 잡기
+        $('#tabsJustified > #li_' + tId + ' a').on('show.bs.tab', function(){
+          alert(tId + ' shown.');
+        });
+
+        
       }
 
       function viewUrl(){
-        alert(22);
+        //alert(22);
         var obj = $("#home1");
         //alert(obj);        
         var url = $("#home1").attr("data-url");
@@ -94,11 +117,9 @@
         });
       }
 
-
-
       function closeTab(tId){
-        //
-        $("").remove();
+        alert("remove : "+ tId);
+        $("#tabs #li_" + tId).remove();
       }
     </script>
   </head>
@@ -106,7 +127,10 @@
   <body onload="initBody()">
     <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
       <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
-      <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+      <div class="w-100 bg-secondary text-right align-middle">
+            <b>홍길동</b>님 어서 오세요. 최근 <b>특정 IP</b>에서 로그인 시간은 
+            <b>2018.06.05 09:10:10</b>입니다.
+      </div>
       <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
           <a class="nav-link" href="#">Sign out</a>
@@ -120,7 +144,7 @@
           <div class="sidebar-sticky">
             <ul class="nav flex-column">
               <li class="nav-item">
-                <a class="nav-link active" href="#">
+                <a class="nav-link active" href="#"  onclick="addTab('aaa','bbbbb','login.php')">
                   <span data-feather="home"></span>
                   Dashboard <span class="sr-only">(current)</span>
                 </a>
@@ -192,10 +216,11 @@
           </div>
         </nav>
 
-        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-2 px-3" style="background-color:green;overflow:visible">
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-0 pb-0 pl-0 pr-0 "
+         style="background-color:green;overflow:visible;">
           
            
-          <div id="tabs" class="h-100" style="background-color:blue;overflow:visible">
+          <div id="tabs" class="h-100" style="background-color:blue;overflow:visible;">
             <ul id="tabsJustified" class="nav nav-tabs">
                 <li class="nav-item"><a href="#home1" data-url="content.php"  data-toggle="tab" class="nav-link small text-uppercase">Home<button class="close" type="button">×</button></a></li>
                 <li class="nav-item"><a href="#profile1" data-url="login.php" data-toggle="tab" class="nav-link small text-uppercase active">Profile</a></li>
@@ -204,15 +229,13 @@
                 login</a></li>
             
             </ul>
-            
-            <br>
             <div id="tabsJustifiedContent" class="tab-content h-100" style="overflow:visible" >
                 <div id="home1" class="tab-pane fade">
                   <input type=button id=a value="addTab" onclick="addTab('aaa','bbbbb')">
                   <input type=button id=a value="viewUrl" onclick="viewUrl()">                           
                     <div class="list-group"><a href="" class="list-group-item d-inline-block"><span class="float-right badge badge-pill badge-dark">51</span> Home Link</a> <a href="" class="list-group-item d-inline-block"><span class="float-right badge badge-pill badge-dark">8</span> Link 2</a>                            <a href="" class="list-group-item d-inline-block"><span class="float-right badge badge-pill badge-dark">23</span> Link 3</a> <a href="" class="list-group-item d-inline-block text-muted">Link n..</a></div>
                 </div>
-                <div id="profile1" class="tab-pane fade active show h-100" style="background:silver;overflow:visible" >
+                <div id="profile1" class="tab-pane fade active h-100" style="background:silver;overflow:visible" >
                   <iframe src="" style="overflow:visible" allowfullscreen="allowFullScreen" frameborder="0" width="100%" height="100%" style="background-color:gray;"></iframe>
                 </div>
                 <div id="messages1" class="tab-pane fade">
@@ -228,16 +251,6 @@
         </main>
       </div>
     </div>
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script
-			  src="https://code.jquery.com/jquery-3.3.1.min.js"
-			  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-			  crossorigin="anonymous"></script>
-        
-    <script src="https://getbootstrap.com/docs/4.0/dist/js/bootstrap.min.js"></script>
 
     <!-- Icons -->
     <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
