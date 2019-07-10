01b1133eb67c57ddfd2dfab4a101cfe3ebfa7e7f
kims man <zero12a@gmail.com>
2019-07-11 07:19:54 +0900

    .

--- login.php
+++ login.php
@@ -20,8 +20,7 @@
     <script src="/lib/jquery/jquery-3.4.1.min.js"></script>
     <script src="/lib/bootstrap4/js/bootstrap.min.js"></script>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-
+   
 
     <!-- 아이콘-->
     <script src="/lib/feather.min.js"></script>
