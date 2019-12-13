1c04aa9ba36acbb84e7a7bcbbd9dc143569d52ff
kims man <zero12a@gmail.com>
2019-12-13 16:27:01 +0900

    logger delete

--- predis_loggerCG.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo Go Logger!
-
-/usr/local/php/bin/php ./predis_loggerCG.php
--- predis_watcher.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo Go Logger Watcher!
-
-cd /data/www/r.d/
-
-/usr/local/php/bin/php ./predis_watcher.php > /dev/null 2>&1 &
