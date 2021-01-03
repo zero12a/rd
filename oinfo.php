7d69bb7d16262a70127254369bcf6bfc732b7caa
kims man <zero12a@gmail.com>
2021-01-03 10:40:16 +0900

    .

--- oinfo.php
+++ oinfo.php
@@ -1,5 +1,20 @@
 <?php
-echo opcache_get_status();
+echo "<pre>";
 
+$t = array();
+$t = opcache_get_status();
+$c = opcache_get_configuration();
+
+//$arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
+
+//echo json_encode($arr);
+
+//var_dump($c);
+
+//var_dump($t);
+
+echo json_encode(json_decode(json_encode($t)), JSON_PRETTY_PRINT);
+
+echo "</pre>";
 //phpinfo();
 ?>
