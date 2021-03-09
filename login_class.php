0284f1946cb8d825662ca8d0042a534c606b33cc
kims man <zero12a@gmail.com>
2021-03-10 08:54:40 +0900

    ldap

---
+++ login_class.php
@@ -0,0 +1,366 @@
+<?php
+
+class loginObject
+{
+
+    private $db;
+
+	//생성자
+	function __construct(){
+        alog("authLog-__construct");
+        global $CFG;
+
+        //DB연결 정보 생성
+        $this->db = getDbConn($CFG["CFG_DB"]["RDCOMMON"]);
+
+	}
+	//파괴자
+	function __destruct(){
+		alog("authLog-__destruct");
+
+        closeDb($this->db);
+    }
+
+    function setAuth(&$objAuth,&$objLogin,$REQ){
+        alog("setAuth().....................start");
+
+        //마지막 로그인세션 기록용(중복로그인 방지)
+        $objAuth->setLastSession($REQ["USR_SEQ"],session_id());
+
+        //권한정보 받아오기
+        $arrAuth = $objLogin->getUserAuthArray($REQ);
+        //var_dump($arrAuth);
+        $objAuth->setUserAuth($arrAuth);
+
+        //로그정보
+        $REQ["AUTH_JSON"] = json_encode($arrAuth);
+        $LoginSeq = $objLogin->saveLoginLog($REQ);
+
+        //인트로 URL가져오기
+        $introUrl = $objLogin->getMyGrpIntroUrl($REQ);
+
+
+        //세션부여
+        setUserSeq($REQ["USR_SEQ"]);
+        setUserId($REQ["USR_ID"]);
+        setUserNm($REQ["USR_NM"]);     
+        setIntroUrl($introUrl);             
+        setLoginSeq($LoginSeq);     
+
+        alog("setAuth().....................end");
+    }
+
+    function getUserInfo($REQ){
+        alog("getUserInfo().....................start");
+        $coltype = "s";
+        $sql = "
+            SELECT
+                USR_SEQ, USR_ID, USR_NM, USR_PWD, PW_ERR_CNT
+                , LOCK_LIMIT_DT, LDAP_LOGIN_YN
+            FROM CMN_USR 
+            WHERE USE_YN = 'Y' and USR_ID = #{F_EMAIL}
+        ";
+        
+        $stmt = makeStmt($this->db,$sql,$coltype,$REQ);
+        
+        if(!$stmt)JsonMsg("500","300","SQL makeStmt 실패 했습니다.");
+        
+        if(!$stmt->execute())JsonMsg("500","100","stmt 실행 실패" . $this->db->errno . " -> " . $this->db->error);
+        
+        alog("getUserInfo().....................end");
+        return $stmt->get_result();        
+
+    }
+
+    function getUserAuthArray($REQ){
+        alog("getUserAuthArray().....................start");
+        //global $db,$REQ;
+    
+        $coltype = "is";
+        $sql = "
+            select b.PGMID, b.AUTH_ID
+            from CMN_GRP_USR a
+                join CMN_GRP_AUTH b on a.GRP_SEQ = b.GRP_SEQ
+                join CMN_MNU c on b.PGMID = c.PGMID
+            where a.USR_SEQ = #{USR_SEQ}
+            and c.PGMTYPE IN (
+                    select PGMTYPE from CMN_IP where ALLOW_IP = #{REMOTE_ADDR} or ALLOW_IP = '0.0.0.0'
+                )
+            order by b.PGMID, b.AUTH_ID
+        ";
+        
+        $stmt = makeStmt($this->db,$sql,$coltype,$REQ);
+        
+        if(!$stmt)JsonMsg("500","300","SQL makeStmt 실패 했습니다.");
+        
+        if(!$stmt->execute())JsonMsg("500","100","stmt 실행 실패" . $this->db->errno . " -> " . $this->db->error);
+        
+    
+        $tArr =  getStmtArray($stmt);
+        $stmt->close();
+    
+        $lastPgmid = "";
+        $rtnVal = null;
+        for($i=0;$i<count($tArr);$i++){
+            $tMap = $tArr[$i];
+            if($lastPgmid != $tMap["PGMID"]){
+                $rtnVal[$tMap["PGMID"]] = array();
+                $j=0;          
+            }else{
+                $j++;        
+            }
+            $rtnVal[$tMap["PGMID"]][$j] = $tMap["AUTH_ID"];
+            $lastPgmid = $tMap["PGMID"];
+        }
+        
+        alog("getUserAuthArray().....................end");
+        return $rtnVal;
+    }
+
+
+    function getMyGrpIntroUrl($REQ){
+        alog("getMyGrpIntroUrl().....................start");
+
+        //global $db,$REQ;
+        $coltype = "i";
+        $sql = "
+            select c.PGMID, c.MNU_SEQ, c.MNU_NM, c.URL
+            from CMN_GRP_USR a
+                join CMN_GRP b on a.GRP_SEQ = b.GRP_SEQ
+                join CMN_MNU c on b.INTRO_PGMID = c.PGMID
+            where a.USR_SEQ = #{USR_SEQ}
+        ";
+        
+        $stmt = makeStmt($this->db,$sql,$coltype,$REQ);
+        
+        if(!$stmt)JsonMsg("500","300","SQL makeStmt 실패 했습니다.");
+        
+        if(!$stmt->execute())JsonMsg("500","100","stmt 실행 실패" . $this->db->errno . " -> " . $this->db->error);
+        
+    
+        $tArr =  getStmtArray($stmt);
+        $stmt->close();
+
+        alog("getMyGrpIntroUrl().....................end");
+        return $tArr;
+    }
+
+
+    function saveLoginLog($REQ){
+        //global $db, $_SERVER;
+        alog("saveLoginLog().....................start");
+
+        $coltype = "sssss isiss ss";
+        $sql = "
+            insert into CMN_LOG_LOGIN ( 
+                USR_ID, SESSION_ID, SUCCESS_YN, RESPONSE_MSG, LOCKCD
+                , PW_ERR_CNT, LOCK_LIMIT_DT, USR_SEQ, SERVER_NAME, REMOTE_ADDR
+                , USER_AGENT, AUTH_JSON
+                , ADD_DT
+            ) values (
+                #{USR_ID}, #{SESSION_ID}, #{SUCCESS_YN}, #{RESPONSE_MSG}, #{LOCKCD}
+                , #{PW_ERR_CNT}, #{LOCK_LIMIT_DT},#{USR_SEQ}, #{SERVER_NAME}, #{REMOTE_ADDR}
+                , #{USER_AGENT}, #{AUTH_JSON}
+                , date_format(sysdate(),'%Y%m%d%H%i%s')
+            ) 
+        ";
+        
+        $stmt = makeStmt($this->db,$sql,$coltype,$REQ);
+        
+        if(!$stmt)JsonMsg("500","300","SQL makeStmt 생성 실패 했습니다.");
+        
+        if(!$stmt->execute())JsonMsg("500","100","stmt 실행 실패 " .  $stmt->error);
+        
+        alog("saveLoginLog().....................end");        
+        return  $db->insert_id;
+    }
+
+
+    //
+    function ldapLoginSuccessUserAdd($REQ){
+        alog("ldapLoginSuccessUserAdd().....................start");
+        //이미 등록된 사용자 인지 확인하기
+
+        $sql = "insert into CMN_USR (
+            USR_ID, USR_NM, PHONE, USE_YN, USR_PWD
+            , PW_ERR_CNT, LAST_STATUS, LOCK_LIMIT_DT, LOCK_LAST_DT, EXPIRE_DT
+            , PW_CHG_DT, PW_CHG_ID, LDAP_LOGIN_YN, TEAMCD, TEAMNM
+            , EMAIL
+            , ADD_DT, ADD_ID
+            ) values (
+                #{USR_ID}, #{USR_NM}, #{PHONE}, 'Y', null
+                ,0, null, null, null, null
+                , null, null, 'Y', #{TEAMCD}, #{TEAMNM}
+                , #{EMAIL}
+                , date_format(sysdate(),'%Y%m%d%H%i%s'), 0
+            )
+            ";
+        $coltype = "sssss s";
+
+        $sqlMap = getSqlParam($sql,$coltype,$req);
+        $stmt = getStmt($this->db,$sqlMap);
+        if(!$stmt->execute())JsonMsg("500","102","(Save usr error) stmt 실행 실패 " .  $stmt->error);
+
+        if($stmt instanceof PDOStatement){
+            alog("SEQYN PDO : " . $this->db->lastInsertId());
+            $newUserSeq = $this->db->lastInsertId(); //insert문인 경우 insert id받기                            
+        }else{
+            alog("SEQYN Mysqli : " . $this->db->insert_id);
+            $newUserSeq = $this->db->insert_id; //insert문인 경우 insert id받기
+        }
+
+        closeStmt($stmt);
+
+        alog("ldapLoginSuccessUserAdd().....................end");
+        return $newUserSeq;
+    }
+
+
+    //
+    function ldapLoginSuccessUserMod($REQ){
+        alog("ldapLoginSuccessUserMod().....................start");
+
+        //이미 등록된 사용자 인지 확인하기
+        $sql = "update CMN_USR set
+                USR_NM = #{USR_NM}, PHONE = #{PHONE}, TEAMCD = #{TEAMCD}, TEAMNM = #{TEAMNM}, EMAIL = #{EMAIL}
+                , PW_ERR_CNT = 0, LOCK_LIMIT_DT = null, LOCK_LAST_DT = null
+                , MOD_DT = date_format(sysdate(),'%Y%m%d%H%i%s'), MOD_ID = 0
+            where USR_SEQ = #{USR_SEQ}
+        ";
+        $coltype = "sssss ssss";
+        
+        $sqlMap = getSqlParam($sql,$coltype,$req);
+        $stmt = getStmt($this->db,$sqlMap);
+        if(!$stmt->execute())JsonMsg("500","102","(Save usr error) stmt 실행 실패 " .  $stmt->error);
+
+        //usr_SEQ 알아오기
+        closeStmt($stmt);
+
+        alog("ldapLoginSuccessUserMod().....................end");
+    }
+
+
+    //비밀번호가 일치한 경우 틀린횟수 클리어
+    function pwClearFailCnt($REQ){
+        alog("pwClearFailCnt().....................start");
+
+        //비번틀림 및 잠금정보 초기화        
+        $coltype = "is";
+        $sql = "
+            UPDATE  CMN_USR SET 
+                PW_ERR_CNT = 0, LOCK_LIMIT_DT = null, LOCK_LAST_DT = null
+                , MOD_DT = date_format(sysdate(),'%Y%m%d%H%i%s')
+                , MOD_ID = #{USR_SEQ}
+            WHERE USR_ID = #{F_EMAIL} 
+        ";
+
+        $stmt = makeStmt($this->db,$sql,$coltype,$REQ);
+        if(!$stmt)JsonMsg("500","300","SQL makeStmt 실패 했습니다.");
+        if(!$stmt->execute())JsonMsg("500","100","stmt 실행 실패" . $stmt->errno . " -> " . $stmt->error);
+
+        alog("pwClearFailCnt().....................end");
+    }
+
+    //패스워드 오류 로직 처리 (PW오류횟수 및 잠금 시간 처리)
+    function loginFailUserAdd($REQ){
+        alog("loginFailUserAdd().....................start");
+        alog("  USR_ID = " . $REQ["USR_ID"]);
+        //등록된 사용자가 아니라고 하면 빈 사용자 사번 추가(pw 실패 기록관리)
+        $REQ["PHONE"] = "";
+        $REQ["EMAIL"] = "";
+        $REQ["TEAMCD"] = "";
+        $REQ["TEAMNM"] = "";
+
+        $sql = "insert into CMN_USR (
+            USR_ID, USR_NM, PHONE, USE_YN, USR_PWD
+            , PW_ERR_CNT, LAST_STATUS, LOCK_LIMIT_DT, LOCK_LAST_DT, EXPIRE_DT
+            , PW_CHG_DT, PW_CHG_ID, LDAP_LOGIN_YN, TEAMCD, TEAMNM
+            , EMAIL
+            , ADD_DT, ADD_ID
+            ) values (
+                #{USR_ID}, #{USR_NM}, #{PHONE}, 'Y', null
+                ,1, null, null, null, null
+                , null, null, 'Y', ifnull(#{TEAMCD},''), ifnull(#{TEAMNM},'')
+                , #{EMAIL}
+                , date_format(sysdate(),'%Y%m%d%H%i%s'), 0
+            )
+            ";
+        $coltype = "sssss s";
+
+
+        $sqlMap = getSqlParam($sql,$coltype,$REQ);
+        $stmt = getStmt($this->db,$sqlMap);
+        if(!$stmt->execute())JsonMsg("500","102","(Save usr error) stmt 실행 실패 " .  $stmt->error);
+        
+        if($stmt instanceof PDOStatement){
+            alog("SEQYN PDO : " . $this->db->lastInsertId());
+            $newUserSeq = $this->db->lastInsertId(); //insert문인 경우 insert id받기                            
+        }else{
+            alog("SEQYN Mysqli : " . $this->db->insert_id);
+            $newUserSeq = $this->db->insert_id; //insert문인 경우 insert id받기
+        }
+
+        return $newUserSeq;
+        alog("loginFailUserAdd().....................end");
+    }
+
+
+
+
+
+    //패스워드 오류 로직 처리 (PW오류횟수 및 잠금 시간 처리)
+    function loginFailUserMod($REQ){
+        alog("loginFailUserMod().....................start");
+        alog("  OLD.PW_ERR_CNT = " . $REQ["PW_ERR_CNT"]);
+
+        //비밀번호가 일치하지 않는 경우 (5번 5분, 10번 10분)
+        $REQ["LOCK_LIMIT_DT"] = "";
+        if($REQ["PW_ERR_CNT"]+1 == 5){
+            $REQ["LOCK_LIMIT_DT"] = date("YmdHis", mktime(date("H"), date("i")+5, date("s"), date("m")  , date("d"), date("Y")) );
+        }else if($REQ["PW_ERR_CNT"]+1 == 10){
+            $REQ["LOCK_LIMIT_DT"] = date("YmdHis", mktime(date("H"), date("i")+10, date("s"), date("m")  , date("d"), date("Y")) );      
+        }else if($REQ["PW_ERR_CNT"]+1 == 15){
+            $REQ["LOCK_LIMIT_DT"] = date("YmdHis", mktime(date("H")+1, date("i"), date("s"), date("m")  , date("d"), date("Y")) );      
+        }else if($REQ["PW_ERR_CNT"]+1 >= 20){
+            $REQ["LOCK_LIMIT_DT"] = date("YmdHis", mktime(date("H"), date("i"), date("s"), date("m")  , date("d")+1, date("Y")) );      
+        }else{
+            //처리 없음
+        }
+
+        $responseMsg = "";
+        if( $REQ["LOCK_LIMIT_DT"] != ""){
+            //[기존유저] 잠금 처리
+            $coltype = "sis";
+            $sql = "
+                UPDATE  CMN_USR SET 
+                    PW_ERR_CNT = PW_ERR_CNT + 1,  LOCK_LIMIT_DT = #{LOCK_LIMIT_DT}
+                    , LOCK_LAST_DT = date_format(sysdate(),'%Y%m%d%H%i%s')
+                    , MOD_DT = date_format(sysdate(),'%Y%m%d%H%i%s')
+                    , MOD_ID = #{USR_SEQ}
+                WHERE USR_ID = #{F_EMAIL} 
+            ";
+            $responseMsg = "ID EXIST, NO LOCK, GO LOCK, PW NOT EQUAL."; 	
+
+
+        }else{
+            //[기존유저] PW 오류 카운트 증가        
+            $coltype = "is";
+            $sql = "
+                UPDATE  CMN_USR SET 
+                    PW_ERR_CNT = PW_ERR_CNT + 1
+                    , MOD_DT = date_format(sysdate(),'%Y%m%d%H%i%s')
+                    , MOD_ID = #{USR_SEQ}
+                WHERE USR_ID = #{F_EMAIL} 
+            ";
+            $responseMsg = "ID EXIST, NO LOCK, GO PW_ERR_CNT, PW NOT EQUAL."; 	
+       
+        }
+
+        $stmt = makeStmt($this->db,$sql,$coltype,$REQ);
+        if(!$stmt)JsonMsg("500","300","SQL makeStmt 실패 했습니다.");
+        if(!$stmt->execute())JsonMsg("500","100","stmt 실행 실패" . $stmt->errno . " -> " . $stmt->error);     
+
+        return $responseMsg;
+    }
+
+}
--- login_ok.php
+++ login_ok.php
@@ -11,10 +11,14 @@ require_once("../common/include/incUtil.php");
 require_once("../common/include/incUser.php");
 require_once("../common/include/incAuth.php");
 require_once("../common/include/incRequest.php");
+require_once('../common/include/incLdap.php');//CG LDAP
 
+
+require_once('./login_class.php');//CG LDAP
 
 //마지막 로그인 세션id기록용
-$objAuth= new authObject();	
+$objAuth = new authObject();	
+$objLogin = new loginObject();	
 
 //외부 파라미터 받기
 $REQ["F_EMAIL"] = reqPostString("F_EMAIL",100);
@@ -31,28 +35,6 @@ $REQ["F_PASSWD_HASH"] = pwd_hash($REQ["F_PASSWD"],$CFG["CFG_SEC_SALT"]);
 
 alog("REQ.F_PASSWD_HASH = ". $REQ["F_PASSWD_HASH"]);
 
-//DB연결 정보 생성
-//$db = db_obj_open(getDbSvrInfo("DATING"));
-
-//DB연결 정보 생성
-$db = getDbConn($CFG["CFG_DB"]["OS"]);
-
-$coltype = "s";
-$sql = "
-    SELECT
-        USR_SEQ, USR_ID, USR_NM, USR_PWD, PW_ERR_CNT
-        , LOCK_LIMIT_DT
-    FROM CMN_USR 
-    WHERE USE_YN = 'Y' and USR_ID = #{F_EMAIL}
-";
-
-$stmt = makeStmt($db,$sql,$coltype,$REQ);
-
-if(!$stmt)JsonMsg("500","300","SQL makeStmt 실패 했습니다.");
-
-if(!$stmt->execute())JsonMsg("500","100","stmt 실행 실패" . $db->errno . " -> " . $db->error);
-
-$result = $stmt->get_result();
 
 //기본정보
 $REQ["REMOTE_ADDR"] = $_SERVER["REMOTE_ADDR"]; 
@@ -60,26 +42,77 @@ $REQ["SERVER_NAME"] = $_SERVER["SERVER_NAME"];
 $REQ["USER_AGENT"] = $_SERVER["HTTP_USER_AGENT"]; 
 $REQ["SESSION_ID"] = session_id(); 
 
-if($row = $result->fetch_array(MYSQLI_NUM))
+$result = $objLogin->getUserInfo($REQ);
+
+//USR_SEQ, USR_ID, USR_NM, USR_PWD, PW_ERR_CNT
+//                , LOCK_LIMIT_DT, LDAP_LOGIN_YN
+
+if($row = $result->fetch_array(MYSQLI_ASSOC))
 {
 
     //아직 잠겨있는 시간인지 확인
     $nowDt = date("YmdHis");
-    if(strlen($row[5]) == 14 && $nowDt <  $row[5]){
+    if(strlen($row["LOCK_LIMIT_DT"]) == 14 && $nowDt <  $row["LOCK_LIMIT_DT"]){
 
         //로그정보
         $REQ["SUCCESS_YN"] = "N";
         $REQ["RESPONSE_MSG"] = "ID EXIST, ID LOCK."; 	
-        $REQ["USR_SEQ"] = 0;
+        $REQ["USR_SEQ"] = $row["USR_SEQ"];
         $REQ["USR_ID"] = $REQ["F_EMAIL"];
-        saveLoginLog($REQ);
+        $objLogin->saveLoginLog($REQ);
 
         //객체 해제
-        $stmt->close();$db->close();unset($objAuth);
+        unset($objAuth);
         
         //RESPONSE 하기
-        JsonMsg("200","220","해당 계정은 잠겨 있습니다. (" . $row[5] . " 까지)");
-    }else if($row[3] == $REQ["F_PASSWD_HASH"]){
+        JsonMsg("200","220","해당 계정은 잠겨 있습니다. (" . $row["LOCK_LIMIT_DT"] . " 까지)");
+    }else if($row["LDAP_LOGIN_YN"] == "Y" && strlen($CFG["CFG_LDAP_HOST"] . "") > 0){ //LDAP_LOGIN_YN
+        //LDAP 로그인 검사
+        $ldap = new ldapClass();
+        $conObj = $ldap->connect($CFG["CFG_LDAP_HOST"]);
+        //echo "<BR>ldap_error : " . ldap_error($conObj);
+
+        if(!$conObj)JsonMsg("500","202","Ldap connect error " .  ldap_error($conObj));
+
+        if( $ldap->login($CFG["CFG_LDAP_DOMAIN"], $REQ["F_EMAIL"],$REQ["F_PASSWD"]) ){
+            //echo "<BR>로그인 성공";
+
+            //ldap서버에서 사용자 정보 조회하기
+            $userLdapMap = $ldap->getUserInfo($CFG["CFG_LDAP_HOST"]);
+
+            //팀 정보는 신규/재로그인시 모두 반영
+            $REQ["USR_ID"] = $REQ["F_EMAIL"];
+            $REQ["USR_NM"] = $userLdapMap["givenname"];
+            $REQ["TEAMNM"] = $userLdapMap["department"];
+            $REQ["TEAMCD"] = $userLdapMap["departmentnumber"];
+
+            $objLogin->ldapLoginSuccessUserMod($REQ);
+
+            //로그인 로그
+            $REQ["SUCCESS_YN"] = "Y";
+            $REQ["RESPONSE_MSG"] = "(LDAP)ID EXIST, PW EQUAL."; 	
+            $REQ["USR_SEQ"] = $row["USR_SEQ"];
+            $REQ["USR_ID"] = $REQ["F_EMAIL"];
+            $objLogin->saveLoginLog($REQ);
+
+        }else{
+            //로그인 실패
+            $REQ["USR_ID"] = $REQ["F_EMAIL"];
+            $REQ["USR_NM"] = "";
+            $REQ["TEAMNM"] = "";
+            $REQ["TEAMCD"] = "";
+            $REQ["PW_ERR_CNT"] = $row["PW_ERR_CNT"];
+
+            $REQ["RESPONSE_MSG"] = $objLogin->loginFailUserMod($REQ);
+
+            //로그인 로그
+            $REQ["SUCCESS_YN"] = "N";
+            $REQ["USR_SEQ"] = $row["USR_SEQ"];
+            $REQ["USR_ID"] = $REQ["F_EMAIL"];
+            $objLogin->saveLoginLog($REQ);
+        }
+
+    }else if($row["USR_PWD"] == $REQ["F_PASSWD_HASH"]){
         //비밀번호가 일치 하는지 검사
         //log("0 : " . $row[0]);
         //alog("1 : " . $row[1]);
@@ -87,233 +120,112 @@ if($row = $result->fetch_array(MYSQLI_NUM))
         //echo "<br>USR_SEQ : " . $row["USR_SEQ"];
 
         //사용자정보 세팅
-        $REQ["USR_SEQ"] = $row[0];
+        $REQ["USR_SEQ"] = $row["USR_SEQ"];
         $REQ["SUCCESS_YN"] = "Y";
         $REQ["RESPONSE_MSG"] = "ID EXIST, NO LOCK, PW EQUAL."; 	            
         $REQ["USR_ID"] = $REQ["F_EMAIL"];
-        $REQ["USR_NM"] = $row[2];
+        $REQ["USR_NM"] = $row["USR_NM"];
 
         //비밀번호 틀린횟수가 1이상이면 비밀에 비밀번호 틀린횟수 및 잠금상태 초기화
-        if($row[4] >= 1){
-            //비번틀림 및 잠금정보 초기화        
-            $coltype = "is";
-            $sql = "
-                UPDATE  CMN_USR SET 
-                    PW_ERR_CNT = 0, LOCK_LIMIT_DT = null, LOCK_LAST_DT = null
-                    , MOD_DT = date_format(sysdate(),'%Y%m%d%H%i%s')
-                    , MOD_ID = #{USR_SEQ}
-                WHERE USR_ID = #{F_EMAIL} 
-            ";
-            $REQ["LOCKCD"] = "UNLOCK";
-
-            $stmt = makeStmt($db,$sql,$coltype,$REQ);
-            if(!$stmt)JsonMsg("500","300","SQL makeStmt 실패 했습니다.");
-            if(!$stmt->execute())JsonMsg("500","100","stmt 실행 실패" . $stmt->errno . " -> " . $stmt->error);
+        if($row["PW_ERR_CNT"] >= 1){
+           $objLogin->pwClearFailCnt($REQ);
         }
 
-        //마지막 로그인세션 기록용(중복로그인 방지)
-        $objAuth->setLastSession($REQ["USR_SEQ"],session_id());
-
-        //권한정보 받아오기
-        $arrAuth = getUserAuthArray();
-        //var_dump($arrAuth);
-        $objAuth->setUserAuth($arrAuth);
-
-        //로그정보
-        $REQ["AUTH_JSON"] = json_encode($arrAuth);
-        $LoginSeq = saveLoginLog($REQ);
-
-        //인트로 URL가져오기
-        $introUrl = getMyGrpIntroUrl();
-
-
-        //세션부여
-        setUserSeq($REQ["USR_SEQ"]);
-        setUserId($REQ["USR_ID"]);
-        setUserNm($REQ["USR_NM"]);     
-        setIntroUrl($introUrl);             
-        setLoginSeq($LoginSeq);     
-
         //객체 해제
-        $stmt->close();$db->close();unset($objAuth);
+        unset($objAuth);
 
         //RESPONSE 하기
         JsonMsg("200","100","로그인에 성공했습니다.");
     }else{
-        
-        //비밀번호가 일치하지 않는 경우 (5번 5분, 10번 10분)
-        $REQ["LOCK_LIMIT_DT"] = "";
-        if($row[4]+1 == 5){
-            $REQ["LOCK_LIMIT_DT"] = date("YmdHis", mktime(date("H"), date("i")+5, date("s"), date("m")  , date("d"), date("Y")) );
-        }else if($row[4]+1 == 10){
-            $REQ["LOCK_LIMIT_DT"] = date("YmdHis", mktime(date("H"), date("i")+10, date("s"), date("m")  , date("d"), date("Y")) );      
-        }else if($row[4]+1 == 15){
-            $REQ["LOCK_LIMIT_DT"] = date("YmdHis", mktime(date("H")+1, date("i"), date("s"), date("m")  , date("d"), date("Y")) );      
-        }else if($row[4]+1 >= 20){
-            $REQ["LOCK_LIMIT_DT"] = date("YmdHis", mktime(date("H"), date("i"), date("s"), date("m")  , date("d")+1, date("Y")) );      
-        }else{
-            //처리 없음
-        }
+        //LDAP유저가 아닌 일반유저 pw가 틀렸을때
+        $REQ["PW_ERR_CNT"] = $row["PW_ERR_CNT"];
+        $objLogin->loginFailUserMod($REQ);
 
-
-        
-        $REQ["USR_SEQ"] = $row[0];
-        if( $REQ["LOCK_LIMIT_DT"] != ""){
-            //잠금 처리
-            $coltype = "sis";
-            $sql = "
-                UPDATE  CMN_USR SET 
-                    PW_ERR_CNT = PW_ERR_CNT + 1,  LOCK_LIMIT_DT = #{LOCK_LIMIT_DT}
-                    , LOCK_LAST_DT = date_format(sysdate(),'%Y%m%d%H%i%s')
-                    , MOD_DT = date_format(sysdate(),'%Y%m%d%H%i%s')
-                    , MOD_ID = #{USR_SEQ}
-                WHERE USR_ID = #{F_EMAIL} 
-            ";
-            $REQ["RESPONSE_MSG"] = "ID EXIST, NO LOCK, GO LOCK, PW NOT EQUAL."; 	
-            $REQ["LOCKCD"] = "GOLOCK";
-            $REQ["PW_ERR_CNT"] = $row[4] + 1;      
-        }else{
-            //PW 오류 카운트 증가        
-            $coltype = "is";
-            $sql = "
-                UPDATE  CMN_USR SET 
-                    PW_ERR_CNT = PW_ERR_CNT + 1
-                    , MOD_DT = date_format(sysdate(),'%Y%m%d%H%i%s')
-                    , MOD_ID = #{USR_SEQ}
-                WHERE USR_ID = #{F_EMAIL} 
-            ";
-            $REQ["PW_ERR_CNT"] = $row[4] + 1;
-            $REQ["RESPONSE_MSG"] = "ID EXIST, NO LOCK, GO PW_ERR_CNT, PW NOT EQUAL."; 	
-        }
-        $stmt = makeStmt($db,$sql,$coltype,$REQ);
-            
-        if(!$stmt)JsonMsg("500","300","SQL makeStmt 실패 했습니다.");
-        
-        if(!$stmt->execute())JsonMsg("500","100","stmt 실행 실패" . $stmt->errno . " -> " . $stmt->error);
 
         //로그정보
         $REQ["SUCCESS_YN"] = "N";
-
-        $REQ["USR_SEQ"] = 0;
+        $REQ["RESPONSE_MSG"] = "ID EQUAL, PW NOT EQUAL."; 
+        $REQ["USR_SEQ"] = $row["USR_SEQ"];
         $REQ["USR_ID"] = $REQ["F_EMAIL"];
-        saveLoginLog($REQ);
-
+        $objLogin->saveLoginLog($REQ);
 
         //객체 해제
-        $stmt->close();$db->close();unset($objAuth);
+        unset($objAuth);
         
         JsonMsg("200","210","이메일(ID) 혹은 비밀번호가 일치하지 않습니다.");
     }
     
 
 }else{
-
-    //로그정보
-    $REQ["SUCCESS_YN"] = "N";
-    $REQ["RESPONSE_MSG"] = "ID NOT EXIST."; 	    
-    $REQ["USR_SEQ"] = 0;
-    $REQ["USR_ID"] = $REQ["F_EMAIL"];
-    saveLoginLog($REQ);
-
-
-
-    //객체 해제
-    $stmt->close();$db->close();unset($objAuth);
-
-    JsonMsg("200","210","이메일(ID) 혹은 비밀번호가 일치하지 않습니다.");    
-}
-
-function getMyGrpIntroUrl(){
-    global $db,$REQ;
-    $coltype = "i";
-    $sql = "
-        select c.PGMID, c.MNU_SEQ, c.MNU_NM, c.URL
-        from CMN_GRP_USR a
-            join CMN_GRP b on a.GRP_SEQ = b.GRP_SEQ
-            join CMN_MNU c on b.INTRO_PGMID = c.PGMID
-        where a.USR_SEQ = #{USR_SEQ}
-    ";
+    //사용자 테이블에 사용자가 없는경우
     
-    $stmt = makeStmt($db,$sql,$coltype,$REQ);
-    
-    if(!$stmt)JsonMsg("500","300","SQL makeStmt 실패 했습니다.");
-    
-    if(!$stmt->execute())JsonMsg("500","100","stmt 실행 실패" . $db->errno . " -> " . $db->error);
-    
+    if(strlen($CFG["CFG_LDAP_HOST"] . "") > 0){ //LDAP_LOGIN_YN
+        //LDAP 로그인 검사
+        $ldap = new ldapClass();
+        $conObj = $ldap->connect($CFG["CFG_LDAP_HOST"]);
+        //echo "<BR>ldap_error : " . ldap_error($conObj);
 
-    $tArr =  getStmtArray($stmt);
-    $stmt->close();
-    return $tArr;
-}
+        if(!$conObj)JsonMsg("500","202","Ldap connect error " .  ldap_error($conObj));
 
-function getUserAuthArray(){
-    global $db,$REQ;
+        if( $ldap->login($CFG["CFG_LDAP_DOMAIN"], $REQ["F_EMAIL"],$REQ["F_PASSWD"]) ){
+            //echo "<BR>로그인 성공";
 
-    $coltype = "is";
-    $sql = "
-        select b.PGMID, b.AUTH_ID
-        from CMN_GRP_USR a
-            join CMN_GRP_AUTH b on a.GRP_SEQ = b.GRP_SEQ
-            join CMN_MNU c on b.PGMID = c.PGMID
-        where a.USR_SEQ = #{USR_SEQ}
-        and c.PGMTYPE IN (
-                select PGMTYPE from CMN_IP where ALLOW_IP = #{REMOTE_ADDR} or ALLOW_IP = '0.0.0.0'
-            )
-        order by b.PGMID, b.AUTH_ID
-    ";
-    
-    $stmt = makeStmt($db,$sql,$coltype,$REQ);
-    
-    if(!$stmt)JsonMsg("500","300","SQL makeStmt 실패 했습니다.");
-    
-    if(!$stmt->execute())JsonMsg("500","100","stmt 실행 실패" . $db->errno . " -> " . $db->error);
-    
+            //ldap서버에서 사용자 정보 조회하기
+            $userLdapMap = $ldap->getUserInfo($CFG["CFG_LDAP_HOST"]);
 
-    $tArr =  getStmtArray($stmt);
-    $stmt->close();
+            //팀 정보는 신규/재로그인시 모두 반영
+            $REQ["USR_ID"] = $REQ["F_EMAIL"];
+            $REQ["USR_NM"] = $userLdapMap["givenname"];
+            $REQ["TEAMNM"] = $userLdapMap["department"];
+            $REQ["TEAMCD"] = $userLdapMap["departmentnumber"];
 
-    $lastPgmid = "";
-    $rtnVal = null;
-    for($i=0;$i<count($tArr);$i++){
-        $tMap = $tArr[$i];
-        if($lastPgmid != $tMap["PGMID"]){
-            $rtnVal[$tMap["PGMID"]] = array();
-            $j=0;          
+            $REQ["USR_SEQ"] = $objLogin->ldapLoginSuccessUserAdd($REQ);
+
+            //로그정보
+            $REQ["SUCCESS_YN"] = "Y";
+            $REQ["RESPONSE_MSG"] = "(LDAP) ID EXIST, NO LOCK, PW EQUAL."; 
+            $REQ["USR_ID"] = $REQ["F_EMAIL"];
+            $objLogin->saveLoginLog($REQ);
+
+            //로그인 성공
+            JsonMsg("200","100","로그인에 성공했습니다.");
+
         }else{
-            $j++;        
+            //로그인 실패
+            $REQ["USR_ID"] = $REQ["F_EMAIL"];
+            $REQ["USR_NM"] = "";
+            $REQ["TEAMNM"] = "";
+            $REQ["TEAMCD"] = "";
+
+            $REQ["USR_SEQ"] = $objLogin->loginFailUserAdd($REQ); //깡통유저 생성
+
+            //로그정보
+            $REQ["SUCCESS_YN"] = "N";
+            $REQ["RESPONSE_MSG"] = "(LDAP) ID OR PW NOT EQUAL."; 
+            $REQ["USR_ID"] = $REQ["F_EMAIL"];
+            $objLogin->saveLoginLog($REQ);
+
+            JsonMsg("200","210","이메일(ID) 혹은 비밀번호가 일치하지 않습니다.(LDAP)");                
         }
-        $rtnVal[$tMap["PGMID"]][$j] = $tMap["AUTH_ID"];
-        $lastPgmid = $tMap["PGMID"];
+    
+    }else{
+        //LDAP사용자도 아니고 존재하지 않는 사용자 일때
+        $REQ["USR_SEQ"]  = $objLogin->loginFailUserAdd($REQ);  //PW오류횟수 및 잠금 시간 처리
+
+        //로그정보
+        $REQ["SUCCESS_YN"] = "N";
+        $REQ["RESPONSE_MSG"] = "ID NOT EXIST."; 	    
+        $REQ["USR_ID"] = $REQ["F_EMAIL"];
+        $objLogin->saveLoginLog($REQ);
+
+        //객체 해제
+        unset($objAuth);
+
+        JsonMsg("200","220","이메일(ID) 혹은 비밀번호가 일치하지 않습니다.");    
     }
-    
-    return $rtnVal;
+
 }
 
-function saveLoginLog($REQ){
-    global $db, $_SERVER;
 
 
-    $coltype = "sssss isiss ss";
-    $sql = "
-        insert into CMN_LOG_LOGIN ( 
-            USR_ID, SESSION_ID, SUCCESS_YN, RESPONSE_MSG, LOCKCD
-            , PW_ERR_CNT, LOCK_LIMIT_DT, USR_SEQ, SERVER_NAME, REMOTE_ADDR
-            , USER_AGENT, AUTH_JSON
-            , ADD_DT
-        ) values (
-            #{USR_ID}, #{SESSION_ID}, #{SUCCESS_YN}, #{RESPONSE_MSG}, #{LOCKCD}
-            , #{PW_ERR_CNT}, #{LOCK_LIMIT_DT},#{USR_SEQ}, #{SERVER_NAME}, #{REMOTE_ADDR}
-            , #{USER_AGENT}, #{AUTH_JSON}
-            , date_format(sysdate(),'%Y%m%d%H%i%s')
-        ) 
-    ";
-    
-    $stmt = makeStmt($db,$sql,$coltype,$REQ);
-    
-    if(!$stmt)JsonMsg("500","300","SQL makeStmt 생성 실패 했습니다.");
-    
-    if(!$stmt->execute())JsonMsg("500","100","stmt 실행 실패 " .  $stmt->error);
-    
-    return  $db->insert_id;
-}
 ?>
